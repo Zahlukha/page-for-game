b348345140981927162950f2103d7e8672ab1bdf
Artem <artemzaglukha@gmail.com>
2017-08-06 13:59:21 +0300

    fix

---
+++ js/common.js
@@ -0,0 +1,26 @@
+$(function() {
+
+	$(document).ready(function(){
+
+		$(function (){
+
+			$("#back-top").hide();
+			
+			$(window).scroll(function (){
+				if ($(this).scrollTop() > 100){
+					$("#back-top").fadeIn();
+				} else{
+					$("#back-top").fadeOut();
+				}
+			});
+
+			$("#back-top a").click(function (){
+				$("body,html").animate({
+					scrollTop:0
+				}, 800);
+				return false;
+			});
+		});
+	});
+
+});
